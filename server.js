4fc21ffff6cda5e9c43e0f061d28dec5396bd7f7
Sam Moran <smoran@wpi.edu>
2020-10-19 19:35:40 -0700

    Added ejs as render engine

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('pages/index')
 })
 
 app.listen(port, () => {
